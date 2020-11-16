28eaedec20348537b17a8e18ca1a8a99b74aedce
Brent Ahrens <cidida@gmail.com>
2020-11-15 18:09:08 -0800

    sets up rest of the toggles

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -36,14 +36,20 @@
           <div class="listen-cover" :class="{show : showListen}">
             <h1>This is the Listen page</h1>
           </div>
+          <div class="download-cover" :class="{show : showDownload}">
+            <h1>This is the Download page</h1>
+          </div>
+          <div class="contact-cover" :class="{show : showContact}">
+            <h1>This is the Contact page</h1>
+          </div>
         </div>
       </div>
       <div class="control-panel">
         <div class="buttons">
           <button @click="listenToggle">Listen</button>
-          <button>Download</button>
+          <button @click="downloadToggle">Download</button>
           <button @click="creditsToggle">Credits</button>
-          <button>Contact</button>
+          <button @click="contactToggle">Contact</button>
         </div>
         <div class="screen">
           <div class="credits" :class="{active : showCredits}">
@@ -100,6 +106,31 @@
               </div>
             </div>
           </div>
+          <div class="download" :class="{show : showDownload}">
+            <div class="download__platform">
+              <div class="image">
+                <img src="@/assets/images/placeholder.jpg" />
+              </div>
+              <div class="image">
+                <img src="@/assets/images/placeholder.jpg" />
+              </div>
+              <div class="image">
+                <img src="@/assets/images/placeholder.jpg" />
+              </div>
+              <div class="image">
+                <img src="@/assets/images/placeholder.jpg" />
+              </div>
+            </div>
+          </div>
+          <div class="contact" :class="{show : showContact}">
+            <div class="contact__platform">
+              <div class="link">
+                <h1>Instagram</h1>
+                <h1>Instagram</h1>
+                <h1>Instagram</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -123,9 +154,27 @@ export default {
       this.showCredits = true
       this.showHome = false
       this.showListen = false
+      this.showDownload = false
+      this.showContact = false
     },
     listenToggle() {
       this.showListen = true
+      this.showCredits = false
+      this.showHome = false
+      this.showDownload = false
+      this.showContact = false
+    },
+    downloadToggle() {
+      this.showDownload = true
+      this.showListen = false
+      this.showCredits = false
+      this.showHome = false
+      this.showContact = false
+    },
+    contactToggle() {
+      this.showContact = true
+      this.showDownload = false
+      this.showListen = false
       this.showCredits = false
       this.showHome = false
     }
